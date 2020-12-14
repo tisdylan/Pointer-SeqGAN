c2b7f8eb70a3161a720e5c6f36a2ef1c831602f4
Architect <31472863+Architect1st@users.noreply.github.com>
2020-12-14 13:19:00 +0800

    Use ground truth

--- gen_trainer.py
+++ gen_trainer.py
@@ -5,6 +5,9 @@ import torch.nn as nn
 from helper import PAD, SOS, EOS, UNK
 from gen_opts import gen_opts
 from torch.autograd import Variable
+# import time
+
+# from rouge import Rouge
 
 unk_id = UNK
 pad_id = PAD
@@ -20,7 +23,7 @@ def get_cuda(tensor):
 
 def gen_trainer(encoder, decoder, encoder_optim, decoder_optim, max_dec_steps, dec_lens,
                 src_seqs, tgt_seqs, src_lens, tgt_lens, enc_lens, extend_vocab_size, target_batch, enc_padding_mask, ct_e, extra_zeros, enc_batch_extend_vocab, USE_CUDA=True): 
-      
+    # time_start=time.time()
     # -------------------------------------
     # Prepare input and output placeholders
     # -------------------------------------
@@ -90,6 +93,8 @@ def gen_trainer(encoder, decoder, encoder_optim, decoder_optim, max_dec_steps, d
     # print("Input into encoder...")
     # encoder_outputs, encoder_hidden = encoder(x, enc_lens)
     # del x
+    
+    # print(src_seqs.cpu().numpy().tolist())
 
     encoder_outputs, encoder_hidden = encoder(src_seqs, enc_lens)
 
@@ -97,6 +102,8 @@ def gen_trainer(encoder, decoder, encoder_optim, decoder_optim, max_dec_steps, d
     # Forward decoder
     # -------------------------------------
     # Initialize decoder's hidden state as encoder's last hidden state.
+    
+    # rouge = Rouge()
 
     step_losses = []
     s_t = (encoder_hidden[0], encoder_hidden[1]) # Decoder hidden states
@@ -105,42 +112,52 @@ def gen_trainer(encoder, decoder, encoder_optim, decoder_optim, max_dec_steps, d
     prev_s = None  # Used for intra-decoder attention (section 2.2 in https://arxiv.org/pdf/1705.04304.pdf)
     sum_temporal_srcs = None  # Used for intra-temporal attention (section 2.1 in https://arxiv.org/pdf/1705.04304.pdf)
     
+    # inds = [] ## for rouge
+    
     # Run through decoder one time step at a time.
     # for t in range(min(max_dec_len, gen_opts.max_decoding_steps)):
     for t in range(max_dec_steps):
         # print("Decoder Steps: ", max_dec_steps)
-
-        # use_gound_truth = get_cuda((torch.rand(len(encoder_outputs)) > 0.25)).long() #Probabilities indicating whether to use ground truth labels instead of previous decoded tokens
-        # x_t = use_gound_truth * target_batch[:, t] + (1 - use_gound_truth) * x_t #Select decoder input based on use_ground_truth probabilities
+        
+        use_gound_truth = get_cuda((torch.rand(len(encoder_outputs)) > 0.25)).long() #Probabilities indicating whether to use ground truth labels instead of previous decoded tokens
+        x_t = use_gound_truth * target_batch[:, t] + (1 - use_gound_truth) * x_t #Select decoder input based on use_ground_truth probabilities
+        is_oov = (x_t >= gen_opts.max_vocab_size).long()  #Mask indicating whether sampled word is OOV
+        x_t = (1 - is_oov) * x_t.detach() + (is_oov) * unk_id  #Replace OOVs with [UNK] token
+        
         x_t = decoder.embedding(x_t)
 
         decoder_output, s_t, ct_e, sum_temporal_srcs, prev_s \
                 = decoder(x_t, s_t, encoder_outputs, enc_padding_mask, ct_e, extra_zeros, enc_batch_extend_vocab, sum_temporal_srcs, prev_s) # 去掉了第四项 src_lens
             
         decoder_outputs[t] = decoder_output
-        #print("Got decoder output. Step: ", t+1, "/", max_dec_steps) # 用来看是不是正常通过的
-        
         target = target_batch[:, t]
-        log_probs = torch.log(decoder_output + eps)
-        step_loss = F.nll_loss(log_probs, target, reduction="none", ignore_index=pad_id)
+        log_probs = get_cuda(torch.log(decoder_output + eps))
+        step_loss = get_cuda(F.nll_loss(log_probs, target, reduction="none", ignore_index=pad_id))
         step_losses.append(step_loss)
 
         x_t = torch.multinomial(decoder_output, 1).squeeze() #Sample words from final distribution which can be used as input in next time step
-        is_oov = (x_t >= gen_opts.max_vocab_size).long()  #Mask indicating whether sampled word is OOV
-        x_t = (1 - is_oov) * x_t.detach() + (is_oov) * unk_id  #Replace OOVs with [UNK] token
-        del decoder_output
+        # ## for rouge
+        # _, x_t_rouge = torch.max(decoder_output, dim=1)
+        # x_t_rouge = x_t_rouge.detach()
+        # inds.append(x_t_rouge)
+        # print(x_t.cpu().numpy().tolist())
+        # print(target.cpu().numpy().tolist())
         
+        del decoder_output
+    # print(inds)
     # -------------------------------------
     # Compute loss
     # -------------------------------------
     # decoder_outputs = decoder_outputs[:max_tgt_len].transpose(0,1).contiguous()
     # tgt_seqs = tgt_seqs.transpose(0,1).contiguous()
     torch.cuda.empty_cache()
+    encoder_optim.zero_grad()
+    decoder_optim.zero_grad()
     # print("\nComputing loss...")
-    losses = torch.sum(torch.stack(step_losses, 1), 1)#unnormalized losses for each example in the batch; (batch_size)
+    losses = get_cuda(torch.sum(torch.stack(step_losses, 1), 1))#unnormalized losses for each example in the batch; (batch_size)
     batch_avg_loss = losses / dec_lens          #Normalized losses; (batch_size)
-    loss = torch.mean(batch_avg_loss)
-    # print("Loss:", loss)
+    loss = get_cuda(torch.mean(batch_avg_loss))
+    # print("Loss:", str(loss))
     
     # -------------------------------------
     # Backward and optimize
@@ -148,12 +165,14 @@ def gen_trainer(encoder, decoder, encoder_optim, decoder_optim, max_dec_steps, d
     # Backward to get gradients w.r.t parameters in model.
     loss.backward()
     # print("\nGot gradient.")
-    
+    # time_end2=time.time()
+    # print('Loss backward time cost: ',time_end2-time_end1,'s')
     # Update parameters with optimizers
     encoder_optim.step()
     decoder_optim.step()
     # print("\nModel updated.")
-
+    torch.cuda.empty_cache()
+    
     num_words = tgt_lens.sum().item()
 
     del src_seqs, tgt_seqs, src_lens, tgt_lens, extend_vocab_size, enc_padding_mask, ct_e, extra_zeros, enc_batch_extend_vocab, s_t, sum_temporal_srcs, prev_s, encoder_outputs, encoder_hidden, target_batch, target, step_losses, log_probs, step_loss
